a1248ab6e998cf053bb1decf7a5910cbd15add7b
gauravkumarchhipa <gauravchhipa295@gmail.com>
2025-04-07 16:19:45 +0530

    env

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import "./styles/index.scss";
 import Home from "./components/Home/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
+  console.log(
+    "API URL:",
+    process.env.REACT_APP_API_URL,
+    "ENV:",
+    process.env.REACT_APP_ENV
+  );
   return (
     <>
       <BrowserRouter>
